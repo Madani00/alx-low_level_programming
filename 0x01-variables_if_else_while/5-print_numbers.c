d208be0f31160689e58280b3cac7eefd8ace3fe6
Madani00 <mamchart@gmail.com>
2023-02-20 05:47:49 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints all single digit numbers of base 10 starting from 0
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	if (i < 10)
+	{
+		printf(i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
