1f019adb90bd191c857ed5c9ead91eb9ceba11d9
Madani00 <mamchart@gmail.com>
2023-02-21 05:49:10 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print 0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i > 47)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
