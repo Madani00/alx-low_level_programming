226d4f9cb7e7db2f0eb5ce4d7af0550e9ac14afa
Madani00 <mamchart@gmail.com>
2023-02-23 03:25:27 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of integer
+ * @n: integer argument
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+}
