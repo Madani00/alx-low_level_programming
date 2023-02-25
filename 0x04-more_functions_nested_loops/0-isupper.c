6a947efe7b0bda1966479707aefaac22bb771d5d
Madani00 <mamchart@gmail.com>
2023-02-25 14:55:32 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to check
+ * Returns 1 if c is uppercase ,Returns 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
