fd83c07b0f6d7ee76bb13a66abe601b6791dd5a9
Madani00 <mamchart@gmail.com>
2023-03-05 13:28:34 -0800

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: one string
+ * @s2: one string
+ * Return: int that tells num spaces in between
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+
+	if (result < 0)
+	{
+		return (*s1 - *s2);
+	}
+	else if (result > 0)
+	{
+		return (*s1 - *s2);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+	return (0);
+}
