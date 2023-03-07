abe6eeda3c198749b43c62a22875c25906843349
Madani00 <mamchart@gmail.com>
2023-03-06 16:22:44 -0800

    string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,11 +13,31 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-	if (i == 0 || isspace(str[i - 1]))
-	{
-		str[i] = toupper(str[i]);
-	}
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
+
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+		{
+			str[i] = str[i] - 32;
+		}
+
 		i++;
 	}
+
 	return (str);
 }
