6e31f4538993204c249afbcb3ce2eb5e8dfba3be
Madani00 <mamchart@gmail.com>
2023-03-22 14:21:54 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int n, sum = 0;
 
-	for (n = 3; n < 10; n++)
+	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
 		{
 			sum = sum + n;
 		}
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 	return (0);
 }
