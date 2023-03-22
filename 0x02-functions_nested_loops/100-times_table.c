eba07e765fcf5a0d990607c1333c7fb905087452
Madani00 <mamchart@gmail.com>
2023-03-22 05:37:58 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,28 +15,27 @@ void print_times_table(int n)
 			for (column = 0; column <= n; column++)
 			{
 				product = (row * column);
-
 				if (column == 0)
 				{
 					_putchar('0' + product);
-				}
-				else if (product <= 9)
+				} else if (product <= 9)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
+				} else if (product > 9 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar('0' + (product / 10));
 					_putchar('0' + (product % 10));
-				}
-				else if (product > 99)
+				} else if (product > 99)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar((product / 100) + '0');
 					_putchar(((product / 10) % 10) + '0');
 					_putchar((product % 10) + '0');
