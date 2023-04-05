b44b18b20feb3156b7a55ddaccfa5dbb6ba897f4
Madani00 <mamchart@gmail.com>
2023-04-05 09:07:05 -0700

    madani

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: string
+ * return : 0
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
