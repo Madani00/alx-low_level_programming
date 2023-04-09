6db65644ec784d84dc51373613b0d3e73f6d1b84
Madani00 <mamchart@gmail.com>
2023-04-08 20:48:10 -0700

    ./nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
