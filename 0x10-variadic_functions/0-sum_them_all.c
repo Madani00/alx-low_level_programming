e69d71f4beb18a89fb5ba75560c6b2c461fe5aa4
Madani00 <mamchart@gmail.com>
2023-04-23 11:07:00 -0700

    great work

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,18 +11,18 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 	
-	va_list mada;
+	va_list args;
 	
 	if (n == 0)
 		return (0);
 
-	va_start(mada, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(mada, int);
+		sum += va_arg(args, int);
 	}
-	va_end(mada);
+	va_end(args);
 
 	return (sum);
 }
