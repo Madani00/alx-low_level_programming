5122595f76b058417ebd56216eb9790ca264b0b5
Madani00 <mamchart@gmail.com>
2023-04-23 11:02:29 -0700

    great work

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters.
@@ -13,6 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	
 	va_list mada;
+	
 	if (n == 0)
 		return (0);
 
