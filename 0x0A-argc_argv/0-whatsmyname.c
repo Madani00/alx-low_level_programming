99f33d1e6a3fa9acc915efe0a7c48693f1d6adda
root <mamchart@gmail.com>
2023-06-09 18:35:21 -0700

    good

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
