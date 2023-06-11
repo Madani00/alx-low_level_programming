36e7e980285f14495c9e842b385be3e5305ba838
root <mamchart@gmail.com>
2023-06-11 10:02:48 -0700

    good

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins
+ * @argc: int
+ * @argv: vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+	int coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (coins = 0; n > 0 ;coins++)
+	{
+		if (n >= 25)
+		{
+			n -= 25;
+		}
+		else if (n >= 10)
+		{
+			n -= 10;
+		}
+		else if (n >= 5)
+		{
+			n -= 5;
+		}
+		else if (n >= 2)
+		{
+			n -= 2;
+		}
+		else if (n >= 1)
+		{
+			n -= 1;
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
