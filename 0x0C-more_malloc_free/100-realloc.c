8fdfe6a22b35904106fef3dcc00999e0ec6405b9
root <mamchart@gmail.com>
2023-06-18 09:40:21 -0700

    advanced: good

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main -  a function that reallocates a memory block using malloc and free
+ * @ptr: a pointer to the memory previously allocated
+ * @old_size: size of the old allocated space
+ * @new_size: size of the new allocated space
+ *
+ * Return: pointer or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size > old_size)
+	{
+		void *new_ptr = malloc(new_size);
+
+		if (ptr != NULL)
+		{
+			memcpy(new_ptr, ptr, old_size);
+		}
+		return (new_ptr);
+	}
+	else
+	{
+		if (new_size == old_size)
+			return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	return (ptr);
+}
