82741bb513029ece3358be9a3e4b3d2dcad4cc6c
root <mamchart@gmail.com>
2023-06-18 11:57:55 -0700

    advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,22 +17,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
-	{
-		void *new_ptr = malloc(new_size);
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	
+	if (new_size == old_size)
+		return (ptr);
 
-		if (ptr != NULL)
-		{
-			memcpy(new_ptr, ptr, old_size);
-		}
-		return (new_ptr);
-	}
-	else
-	{
-		if (new_size == old_size)
-			return (ptr);
-	}
 	free(ptr);
 	ptr = malloc(new_size);
+
 	return (ptr);
 }
