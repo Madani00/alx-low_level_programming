dfb0a8140df37981de3d258073288b6ecb95da63
root <mamchart@gmail.com>
2023-06-19 02:33:22 -0700

    advanced:good

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,17 +7,21 @@
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num1, num2;
+int num1, num2, result;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	printf("%d\n", num1 * num2);
-	return (0);
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+else
+{
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
+printf("%d\n", result);
+return (0);
+}
 }
