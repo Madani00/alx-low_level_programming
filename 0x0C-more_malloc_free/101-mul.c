81ac0a8f599b572930c6d58ce1c9a5ca77ece6d9
root <mamchart@gmail.com>
2023-06-19 02:19:56 -0700

    advanced: very good

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
 	return (0);
 }
