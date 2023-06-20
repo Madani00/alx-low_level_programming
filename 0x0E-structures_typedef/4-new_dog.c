9d86cf72e7110f3140b9c50707c23c85924e73b6
root <mamchart@gmail.com>
2023-06-20 05:41:46 -0700

    good

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strcopy - Copies a string pointed to src, including the
+ *            terminating null byte, to a buffer pointed  by dest.
+ * @dest: stores the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+
+char *_strcopy(char *des, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		des[i] = src[i];
+
+	des[i] = '\0';
+	return (des);
+}
+/**
+ * *new_dog - a function that creates a new dog.
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: NULL or my_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->name = _strcopy(d->name, name);
+	d->age = age;
+	d->owner = _strcopy(d->owner, owner);
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,8 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcopy(char *des, char *src);
 void free_dog(dog_t *d);
 
 #endif
