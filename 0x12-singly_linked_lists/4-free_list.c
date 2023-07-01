97f39d3f7773d796eed999a694837d338a542027
root <mamchart@gmail.com>
2023-07-01 07:15:52 -0700

    great work

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,14 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp = head;
+	list_t *temp;
+
+	if (head == NULL)
+		return;
 
 	while (head != NULL)
 	{
+		temp = head;
 		head = head->next;
 		free(temp);
 	}
