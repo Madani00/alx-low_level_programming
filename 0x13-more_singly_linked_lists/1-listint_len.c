0a47efefee1363874bf32687f620925dc5f59325
root <mamchart@gmail.com>
2023-07-04 05:43:29 -0700

    great work

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer points to head
+ *
+ * Return: length
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
