525acf43d03b5b406935d9e5d3f25d8159e7f9f3
root <mamchart@gmail.com>
2023-07-06 18:10:51 -0700

    good

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return:  converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a;
+	int len, base_two;
+
+	if (!b)
+		return (0);
+
+	a = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			a += base_two;
+		}
+	}
+	return (a);
+}
