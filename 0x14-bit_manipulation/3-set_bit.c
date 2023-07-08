fd8aab0f73624a2460cc3e4f2aeb66fa3aead5da
root <mamchart@gmail.com>
2023-07-07 18:57:31 -0700

    hard

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to 1 at given index.
+ * @n: unsigned long to change.
+ * @index: change to one
+ * 
+ * Return: 1 if it worked , -1 if error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	(*n |= 1 << index);
+	return (1);
+}
