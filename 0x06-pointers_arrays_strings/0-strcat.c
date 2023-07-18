da62b449bd363e7d007f8c28e9a0adb754c0e70e
Madani00 <123333191+Madani00@users.noreply.github.com>
2023-07-18 14:14:32 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,3 +17,21 @@ char *_strcat(char *dest, char *src)
 	strcat(dest, src);
 	return (dest);
 }
+
+/**
+*
+* another way to do it.
+*
+* char *_strcat(char *dest, char *src)
+* {
+*	char *ret = dest;
+*
+*	while (*dest)
+*		dest++;
+*	while (*src)
+*		*dest++ = *src++;
+*	*dest = *src;
+*	return (ret);
+* }
+*
+*/
