a799d0a1c95b2d0e85d61aebe2dcc1c81e8906d8
Madani00 <mamchart@gmail.com>
2023-08-08 12:32:48 -0700

    very good

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,45 +16,52 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	copy_file(av[1], av[2]);
+	_cp(av[1], av[2]);
 
 	return (0);
 }
 
 /**
- * copy_file - copies content from one file to another.
+ * _cp - copies content from one file to another.
  * @src: the file to copy from
  * @dest: destination
  */
 
-void copy_file(const char *src, const char *dest)
+void _cp(const char *src, const char *dest)
 {
 	char buffer[1024];
 	int from, to;
 	ssize_t w, r;
 
 	from = open(src, O_RDONLY);
-	r = read(from, buffer, sizeof(buffer));
-	if (r == -1 || from == -1)
+	r = read(from, buffer, 1024);
+	to = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while (r > 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", src);
-		exit(98);
-	}	
-	to = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	w = write(to, buffer, r);
-	if (w == -1 || to == -1)
-	{	
-		dprintf(2, "Error: Can't write to %s\n", dest);
-		exit(99);
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			exit(99);
+		}
 	}
+
 	if (close(from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd FD_VALUE %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
 		exit(100);
 	}
+
 	if (close(to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd FD_VALUE %d\n", to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
 		exit(100);
-	}	
+	}
 }
