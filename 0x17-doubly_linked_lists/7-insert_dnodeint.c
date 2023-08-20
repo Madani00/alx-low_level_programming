a15d91306ccdb3c8f491e741605c78f3e9b0f641
Madani00 <Madani00>
2023-08-20 14:55:23 -0700

    well done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 
+	if (temp->next == NULL)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
 	while (idx != 1)
 	{
 		temp = temp->next;
@@ -30,5 +36,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = temp;
 	new_node->next = temp2;
 	temp2->prev = new_node;
-	return (*h);
+	return (new_node);
 }
