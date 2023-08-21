b8cdc666d54c06af9a9d0fcf806fc881650d3ad2
Madani00 <Madani00>
2023-08-20 19:17:36 -0700

    good

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes node at index 
+ * delete_dnodeint_at_index - deletes node at index
  * @head: pointer to the head node
  * @index: index of the node to be deleted
  *
