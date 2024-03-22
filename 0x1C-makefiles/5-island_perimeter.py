99aabd86eb6a7516dc39e1e073f584b34e61bdb4
Madani00 <mamchart@gmail.com>
2024-03-21 22:02:01 -0700

    NiiiiiiiiiiiiiiiiiiiiiiCe

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+if value is 1 add 4 to the perimeter and then checks neighboring cells
+If there is a neighboring cell to the top or left, subtracts 2
+"""
+
+
+def island_perimeter(grid):
+    prim = 0
+
+    for i in range(len(grid)):
+        for k in range(len(grid[0])):
+            if grid[i][k] == 1:
+                prim += 4
+                if k > 0 and grid[i][k-1] == 1:
+                    prim -= 2
+                if grid[i][k] == 1 and grid[i-1][k] == 1:
+                    prim -= 2
+    return prim
