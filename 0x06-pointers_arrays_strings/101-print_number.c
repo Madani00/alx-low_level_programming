02b7788a12856acdf7d523ad06aba942e5ad860c
El Madani <123333191+Madani00@users.noreply.github.com>
2024-06-18 11:18:44 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -21,3 +21,19 @@ void print_number(int n)
 
 	_putchar((num % 10) + '0');
 }
+
+// my way
+/*
+void print_number(int n)
+{
+    if (n < 0)
+	{
+		putchar('-');
+		n = n*-1;
+	}
+    if (n >= 10) {
+        print_number(n / 10);
+    }
+    putchar((n % 10) + '0');
+}
+*/
